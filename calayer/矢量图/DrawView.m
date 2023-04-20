4f2b07f2319f8daa4098fac68a3f8bfd651a0497
xu qianlan <xuqianlan@xudeMacBook-Pro.local>
2023-04-20 10:12:34 +0800

    [Mod]避免重绘任何旧的线刷以不至于覆盖已更新过的区域。这样做会显著地提高绘制效率

--- calayer/矢量图/DrawView.m
+++ calayer/矢量图/DrawView.m
@@ -39,12 +39,21 @@
     [self setNeedsDisplay];
 }
 
+- (CGRect)brushRectForPoint:(CGPoint)point {
+    return CGRectMake(point.x - BRUSH_SIZE * 0.5, point.y - BRUSH_SIZE * 0.5, BRUSH_SIZE, BRUSH_SIZE);
+}
+
 - (void)drawRect:(CGRect)rect {
     for (NSValue *value in _strokes) {
         CGPoint point = value.CGPointValue;
         
-        CGRect brushRect = CGRectMake(point.x - BRUSH_SIZE * 0.5, point.y - BRUSH_SIZE * 0.5, BRUSH_SIZE, BRUSH_SIZE);
-        [[UIImage imageNamed:@"Chalk"] drawInRect:brushRect];
+        CGRect brushRect = [self brushRectForPoint:point];
+        
+        // 避免重绘任何旧的线刷以不至于覆盖已更新过的区域。这样做会显著地提高绘制效率
+        if(CGRectIntersectsRect(rect, brushRect)) {
+            [[UIImage imageNamed:@"Chalk"] drawInRect:brushRect];
+        }
+        
     }
 }
 @end
